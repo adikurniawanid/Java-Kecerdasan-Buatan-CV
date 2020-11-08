a0b1508b9c963f8a3f54b5e9d2da4125bfce1e9c
Adi Kurniawan <adikurniawan.dev@gmail.com>
2020-11-08 09:24:51 +0700

    Update MainProgram.java
    menambahkan HighGui.imgshow untuk menampilkan gambar dalam bentuk gui

--- src/id/akdev/jopencv/MainProgram.java
+++ src/id/akdev/jopencv/MainProgram.java
@@ -1,35 +1,33 @@
 package id.akdev.jopencv;
 
 import org.opencv.core.Core;
+import org.opencv.core.Scalar;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
+import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
-public class MainProgram {
-    public static void main(String[] args) {
+public class MainProgram{
+    public static void main(String[] args){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        String imgFile = "images/kabeshaJKT48.jpg";
-        Mat src = Imgcodecs.imread(imgFile);
-
-        String xmlFile = "xml/lbpcascade_frontalface.xml";
-        CascadeClassifier cc = new CascadeClassifier(xmlFile);
+        Mat gambar = Imgcodecs.imread("images/gambar2.png");
+        CascadeClassifier cc = new CascadeClassifier("xml/lbpcascade_frontalface.xml");
 
         MatOfRect faceDetection = new MatOfRect();
-        cc.detectMultiScale(src, faceDetection);
-        System.out.println(String.format("Jumlah Wajah Oshi Terdeteksi: %d", faceDetection.toArray().length));
+        cc.detectMultiScale(gambar, faceDetection);
 
         for(Rect rect: faceDetection.toArray()){
-            Imgproc.rectangle(src, new Point(rect.x , rect.y),
-                    new Point(rect.x + rect.width,rect.y + rect.height)
-                    ,new Scalar(0, 0, 255), 3);
+            Imgproc.rectangle(gambar, new Point(rect.x , rect.y),
+                    new Point(rect.x + rect.width,rect.y + rect.height),
+                    new Scalar(0, 255, 0), 2);
         }
 
-        Imgcodecs.imwrite("images/kabeshaJKT48_out.jpg", src);
-        System.out.println("Image Detection Selesai");
+        Imgcodecs.imwrite("images/gambarHasil.png", gambar);
+        HighGui.imshow("Jumlah wajah terdeteksi : " + faceDetection.toArray().length + " wajah", gambar);
+        HighGui.waitKey();
     }
 }
